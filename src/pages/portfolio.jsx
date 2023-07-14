6f1fe04940428ee4a7554f6de089398c5bb9c0c5
Kris Bunuabenjamas <titykris7174@gmail.com>
2023-07-15 02:03:31 +0700

    bug

--- src/pages/portfolio.jsx
+++ src/pages/portfolio.jsx
@@ -4,10 +4,40 @@ import './css/portfolio.css'
 
 function Portfolio(){
     return(
+        <>
         <div>
             <NavBar></NavBar>
-            This is Portfolio
         </div>
+        <div className="Leftzone-portfolio">
+                <div className="user-port">
+                    <img id="user-housePort" className="User-pro" src="https://images.unsplash.com/photo-1633633292416-1bb8e7b2832b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8M2QlMjBob3VzZXxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt=""/>
+                </div>
+        </div>
+        <div className="Rightzone-portfolio">
+            <h1 className="port-text" id="CO2-num">50</h1>
+            <p1 className="port-text">tCO2eqv ถูกกำจัด</p1>
+            <p1 className="port-text">= 40 ต้น </p1>
+            <p1 className="port-text">Green points : 2,000 points </p1>
+            <nav class="history-bar">
+                <h1><pre>     History                                <button>Certificate</button></pre></h1>
+                <div className="historybox">
+                    <div className="historybox-inner">
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                        <p> &emsp; 54CC &emsp; &emsp; &emsp; &emsp; 14.14น. 30/7/2566</p>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+        </>
     )
 }
 
