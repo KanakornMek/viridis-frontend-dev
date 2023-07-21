bd7ec008be65beb19d129ab58d3e682a8c2605ef
Nattapon Howong <74097224+understann@users.noreply.github.com>
2023-07-21 16:08:14 +0700

    Create payment page

---
+++ src/pages/payment-test.jsx
@@ -0,0 +1,13 @@
+import NavBar from "../components/navbar";
+
+
+function Payment(){
+    return(
+        <div>
+            <NavBar></NavBar>
+            This is Portfolio
+        </div>
+    )
+}
+
+export default Portfolio;
