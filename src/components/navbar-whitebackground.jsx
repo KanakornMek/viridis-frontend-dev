4ad677dbb78e989f1ce60f0aabefaaf89757e56c
Nattapon Howong <nattapon.how@gmail.com>
2023-07-25 17:57:18 +0700

    Create Services page
    :sparkles: Finish Services Page 1 out of 2 with responsive feature

---
+++ src/components/navbar-whitebackground.jsx
@@ -0,0 +1,79 @@
+import '../components/navbar-whitebackground.css'
+import { Link, useNavigate, redirect } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import  useScreenSize from './screenSizeHook'
+
+
+function NavBarwhitebackground(){
+    const [isGoingToLogin,setisGoingToLogin] = useState(false);
+    const navigate = useNavigate();
+    const [isNavOn, setisNavOn] = useState(false);
+
+    useEffect(() =>{
+        console.log(isGoingToLogin)
+        if(isGoingToLogin){
+            navigate('/login');
+        }
+
+    },)
+
+    window.addEventListener("resize", function() {
+        if (window.matchMedia("(min-width: 800px)").matches) {
+            document.getElementById('hammy').classList.remove("change");
+            document.getElementById('nav-on-screen').className = 'nav-on-screen'
+            document.getElementById('blackdrop').className = 'blackdrop'
+            setisNavOn(false);
+        }
+      })
+
+    useEffect(() => {
+        if(isNavOn){
+            document.getElementById('hammy').classList.add("change");
+            document.getElementById('nav-on-screen').className += '-on'
+            document.getElementById('blackdrop').className += '-on'
+            document.getElementById('body').className += 'fixForHam'
+        }else{
+            document.getElementById('hammy').classList.remove("change");
+            document.getElementById('nav-on-screen').className = 'nav-on-screen'
+            document.getElementById('blackdrop').className = 'blackdrop'
+            document.getElementById('body').className = ''
+        }
+    }, [isNavOn])
+
+
+    return(
+        <div className='navBar' id='navBar'>
+            <div className='leftSideNav1'>
+                <a>viridis.</a>
+            </div>
+            <div className="blackdrop" id='blackdrop'></div>
+            <div className='rightSideNav'>
+                <ul className='innerRightSideNav'>
+                    <li><Link to={`/`} className='home1'>Home</Link></li>
+                    <li><Link to={`/portfolio`} className='portfolio1'>Portfolio</Link></li>
+                    <li><Link to={`/services`} className='services1'>Services</Link></li>
+                </ul>
+                <div className='changableNavComponent'>
+                    <button onClick={() => setisGoingToLogin(true)}>Login</button>
+                </div>
+            </div>
+            <div class="hammy" id='hammy' onClick={() => setisNavOn(!isNavOn)}>
+                <div class="bar01"></div>
+                <div class="bar02"></div>
+                <div class="bar03"></div>
+            </div>
+            <div className="nav-on-screen" id='nav-on-screen'>
+                <ul className='navTextOnScreen'>
+                    <li><Link to={`/`} className='home'>Home</Link></li>
+                    <li><Link to={`/portfolio`} className='portfolio'>Portfolio</Link></li>
+                    <li><Link to={`/services`} className='services'>Services</Link></li>
+                </ul>
+                <div className='buttonOnScreen'>
+                    <button onClick={() => setisGoingToLogin(true)}>Login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBarwhitebackground;
--- src/pages/services.jsx
+++ src/pages/services.jsx
@@ -1,10 +1,20 @@
-import NavBar from "../components/navbar";
+import NavBarwhitebackground from "../components/navbar-whitebackground";
+import './css/services.css'
 
 function Services(){
     return(
         <div>
-            <NavBar></NavBar>
-            This is services page!
+            <NavBarwhitebackground></NavBarwhitebackground>
+            <div className="service-container">
+                <div className="leftside-service">
+                    <div className="text-container">
+                        <h1>Let your customer easily offset the carbon footprint</h1>
+                    </div>
+                    <button className="actnow-button"><span>ACT NOW</span></button>
+                </div>
+                <div className="rightside-service"></div>
+            </div>
+
         </div>
     )
 }
